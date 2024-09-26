ccd7d600369a8e61a9bb56174077e6be8a61b625
Dipesh <dipesh@example.com>
2024-09-26 22:37:40 +0545

    First Ts session

---
+++ script.js
@@ -0,0 +1,36 @@
+// let a:boolean ;
+// let arr:[];
+// let str:string='hey'
+// let boolean:true
+// let arr:[boolean,number,string]=[false,12,"haha"];
+// let variable:unknown;
+// variable =12;
+// variable ='hey';
+// function abcd():string{
+//     return 'LOL';
+// }
+// abcd().toLocaleLowerCase
+//function that doesn't return anything:
+// function bcdef():void{
+//     console,log('hey')
+// }
+//enums
+// enum Direction {
+//     top ='TOP',
+//     left="LEFT",
+//     right="RIGHT",
+//     bottom ='BOTTOM',
+// }
+// Direction.bottom
+var MousePosition;
+(function (MousePosition) {
+    MousePosition[MousePosition["x"] = 0] = "x";
+    MousePosition[MousePosition["y"] = 0] = "y";
+})(MousePosition || (MousePosition = {}));
+MousePosition.x;
+var Human;
+(function (Human) {
+    Human["name"] = "harsh";
+    Human[Human["age"] = 25] = "age";
+})(Human || (Human = {}));
+console.log(Human.name);
---
+++ script.ts
@@ -0,0 +1,63 @@
+// let a:boolean ;
+
+// let arr:[];
+
+// let str:string='hey'
+
+// let boolean:true
+
+
+// let arr:[boolean,number,string]=[false,12,"haha"];
+
+// let variable:unknown;
+
+// variable =12;
+// variable ='hey';
+
+
+// function abcd():string{
+//     return 'LOL';
+// }
+
+// abcd().toLocaleLowerCase
+
+
+//function that doesn't return anything:
+
+// function bcdef():void{
+//     console,log('hey')
+// }
+
+
+//enums
+
+
+// enum Direction {
+//     top ='TOP',
+//     left="LEFT",
+//     right="RIGHT",
+//     bottom ='BOTTOM',
+
+// }
+
+// Direction.bottom
+
+
+
+
+
+enum MousePosition{
+    x=0,
+  y=0,
+}
+
+MousePosition.x
+//enum helps to group things
+
+enum Human{
+    name='harsh',
+    age=25
+}
+
+
+console.log(Human.name)
